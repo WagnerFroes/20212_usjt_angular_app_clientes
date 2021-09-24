43f69679eb27810b8fd549c3ce0fb91da469e0c3
WagnerFroes <lucaswagnerp@gmail.com>
2021-09-23 22:21:14 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClienteInserirComponent } from './cliente/cliente-inserir/cliente-inserir.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteInserirComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
